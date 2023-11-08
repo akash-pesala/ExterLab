ab306882386cfacd8c069cfb6e89be28a7b99d64
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 11:21:14 +0530

    committed!

--- CircleRecCub/src/Areas.java
+++ CircleRecCub/src/Areas.java
@@ -1,9 +1,17 @@
-
+import java.util.*;
 public class Areas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+          int length=sc.nextInt();
+          int Breadth=sc.nextInt();
+          RectangleArea(length,Breadth);
+          
 	}
 
+	public static void RectangleArea(int l, int b)
+	{
+		System.out.println(l*b);
+	}
 }
